977b7a51725cf3eb7a8a701263a337aa2499bd3b
Josh Olliff <99947006+josholliff@users.noreply.github.com>
2026-08-14 09:00:26 -0700

    phase 1/policy definitions (#5)
    * Phase 1: management group hierarchy and ADR 0002
    
    * Phase 1: custom policy definitions at tenant root

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -28,3 +28,14 @@ module "management_groups" {
   root_parent_id  = "/providers/Microsoft.Management/managementGroups/${data.azurerm_client_config.current.tenant_id}"
   subscription_id = data.azurerm_client_config.current.subscription_id
 }
+module "policy" {
+  source = "../../modules/policy"
+
+  org_code            = "jbo"
+  definition_scope_id = "/providers/Microsoft.Management/managementGroups/${data.azurerm_client_config.current.tenant_id}"
+  landing_zones_id    = module.management_groups.landing_zones_id
+  lz_corp_id          = module.management_groups.lz_corp_id
+  location            = "eastus2"
+
+  enable_assignments = false
+}
